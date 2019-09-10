07c4a8c94c83da98aeafcd94db487d5866cabc97
lilimin <290059742@qq.com>
2019-09-10 20:56:56 +0800

    完善spring cache示例项目

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## spring cache使用demo
+master分支为不使用spring cache的做法  
+spring-cache分支为spring cache的详细用法
 
 
 
--- src/main/java/com/st/Main.java
+++ src/main/java/com/st/Main.java
@@ -7,23 +7,11 @@ public class Main {
   public static void main(String[] args) {
 
     /**
-     * get from db somebody
-     * get from cache somebody
-     *
-     * after reload...
-     *
-     * get from db somebody
-     * get from cache somebody
+     * get from db aaa
+     * get from cache aaa
      */
     AccountService s = new AccountService();
-    // 开始查询账号
-    s.getAccountByName("somebody");
-    s.getAccountByName("somebody");
-
-    s.reload();// 重置缓存
-    System.out.println("after reload...");
-
-    s.getAccountByName("somebody");
-    s.getAccountByName("somebody");
+    s.getAccountByName("aaa");
+    s.getAccountByName("aaa");
   }
 }
