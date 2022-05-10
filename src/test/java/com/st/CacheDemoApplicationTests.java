d8351a336c33eb998bd80e645552941cb1e30beb
li <290059742@qq.com>
2022-05-10 10:38:59 +0800

    增加测试的demo

--- src/test/java/com/st/CacheDemoApplicationTests.java
+++ src/test/java/com/st/CacheDemoApplicationTests.java
@@ -1,16 +1,28 @@
 package com.st;
 
+import com.javashitang.CacheDemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {CacheDemoApplication.class})
 public class CacheDemoApplicationTests {
+
+	@Value("${show.test}")
+	private List<Integer> showList;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void t1() {
+		System.out.println(showList);
 	}
 
 }
